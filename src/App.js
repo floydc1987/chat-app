2baabad91c9782303f22b5ef4a94d2b74f27f43c
Celina Floyd <celinaleann@gmail.com>
2018-10-11 01:57:47 -0700

    added room list features for chat

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './Components/RoomList.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    var config = {
+      apiKey: "AIzaSyANV6z00XWcKoZ7anjgADmALq2G1MqVO1s",
+      authDomain: "chat-app-ef3c3.firebaseapp.com",
+      databaseURL: "https://chat-app-ef3c3.firebaseio.com",
+      storageBucket: "gs://chat-app-ef3c3.appspot.com"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <RoomList firebase={firebase} />
       </div>
     );
   }
-}
+};
 
 export default App;
